5a35f366e9b79fb8459d4686fbcd7ef1df114d78
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-03-11 22:56:54 -0400

    chore: recover tests

--- src/test/encode_test.py
+++ src/test/encode_test.py
@@ -3,21 +3,21 @@ from dataclasses import dataclass
 
 
 test_cases = [
-    # [1, 2, 3, 4], # simple list
-    # [1, 2.3, "a", None, True, False, [], {}], # list
+    [1, 2, 3, 4], # simple list
+    [1, 2.3, "a", None, True, False, [], {}], # list
     ("a", 1, 2.3, 2.3, None, True, False, [], {}), # tuple
     tuple(range(100)), # large tuple
     "a", # simple string
     1, # simple int
-    # 2.3, # simple float
-    # math.inf, math.nan, math.pi, # TODO pi and other long float will cause SEGV
-    # None,
-    # True,
-    # False,
-    # [],
-    # {},
-    # dict({a: b for a in range(10) for b in range(10)}), # set
-    # 321321432.23154324, # large float # TODO assert fail in decimal part length
+    2.3, # simple float
+    math.inf, math.nan, math.pi,
+    None,
+    True,
+    False,
+    [],
+    {},
+    dict({a: b for a in range(10) for b in range(10)}), # set
+    321321432.231543245, # large float # TODO assert fail in decimal part length
 ]
 for case in test_cases:
     print("Case:", case, type(case))
