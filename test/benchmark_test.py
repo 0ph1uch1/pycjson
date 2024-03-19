5c980483987ee0f6818bcaabf34aab2ec192fd64
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-03-18 21:23:17 -0400

    chore: update benchmark for decode

--- test/benchmark_test.py
+++ test/benchmark_test.py
@@ -4,7 +4,7 @@ import unittest
 
 
 class TestBenchmark(unittest.TestCase):
-    def test_benchmark(self):
+    def test_benchmark_encode(self):
         import json
         import cjson
 
@@ -13,6 +13,8 @@ class TestBenchmark(unittest.TestCase):
             "bench"
         )
 
+        print("test_benchmark_encode start")
+
         for _file in os.listdir(benchmark_folder):
             filename = os.path.join(benchmark_folder, _file)
             with open(filename, "r") as f:
@@ -20,6 +22,26 @@ class TestBenchmark(unittest.TestCase):
             time_std = self.time_benchmark(1000, json.dumps, data, indent=None, ensure_ascii=False)
             print(f"file: {_file}, time_std: {time_std}")
             time_cjson = self.time_benchmark(1000, cjson.dumps, data)
+            print(f"file: {_file}, time_cjson: {time_cjson}")
+
+    def test_benchmark_decode(self):
+        import json
+        import cjson
+
+        benchmark_folder = os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            "bench"
+        )
+
+        print("test_benchmark_decode start")
+
+        for _file in os.listdir(benchmark_folder):
+            filename = os.path.join(benchmark_folder, _file)
+            with open(filename, "r") as f:
+                data = f.read()
+            time_std = self.time_benchmark(1000, json.loads, data)
+            print(f"file: {_file}, time_std: {time_std}")
+            time_cjson = self.time_benchmark(1000, cjson.loads, data)
             print(f"file: {_file}, time_cjson: {time_cjson}")
 
     def time_benchmark(self, repeat_time, func, *args, **kwargs):
