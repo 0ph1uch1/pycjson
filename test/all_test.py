43348a7067e8be30dee7c674d919ee5a6567a23b
Antares <Antares0982@gmail.com>
2024-03-30 23:23:08 +0800

    test: Add support for --only flag in test script

--- test/all_test.py
+++ test/all_test.py
@@ -9,8 +9,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # ignore list
     parser.add_argument("--ignore", nargs="+", default=[])
+    parser.add_argument("--only", nargs="+", default=[])
     args = parser.parse_args()
     ignores = set(args.ignore)
+    onlys = set(args.only)
+    if ignores and onlys:
+        print("Cannot use both --ignore and --only")
+        sys.exit(1)
     #
     cur_path = os.path.dirname(os.path.abspath(__file__))
     if cur_path not in sys.path:
@@ -21,7 +26,10 @@ if __name__ == "__main__":
     for file in modules:
         if file.endswith(".py") and file != "__init__.py" and os.path.join(cur_path, file) != os.path.abspath(__file__):
             name = file[:-3]
-            if name in ignores or (name.endswith("_test") and name[:-5] in ignores):
+            if onlys and name not in onlys and (name.endswith("_test") and name[:-5] not in onlys):
+                print(f"Skipping {name}")
+                continue
+            if ignores and (name in ignores or (name.endswith("_test") and name[:-5] in ignores)):
                 print(f"Skipping {name}")
                 continue
             module = importlib.import_module(file[:-3])
