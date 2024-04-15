6deaad117956802f8b32041a18b2fdfc5911ac07
helix_xx <630824252@qq.com>
2024-04-16 02:37:44 +0800

    #4 add test case for load and dump

---
+++ test/file_test.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import cjson
+import json
+import unittest
+
+class TestJsonFileIO(unittest.TestCase):
+    def setUp(self):
+        self.test_data = {
+            "name": "XXY",
+            "age": 99,
+            "city": "GZ"
+        }
+        self.filepath = "test_data.json"
+
+    def tearDown(self):
+        if os.path.exists(self.filepath):
+            os.remove(self.filepath)
+
+    def test_json_load(self):
+        with open(self.filepath, "w") as f:
+            json.dump(self.test_data, f)
+
+        with open(self.filepath, "r") as f:
+            loaded_data = cjson.load(f)
+        
+        self.assertEqual(loaded_data, self.test_data)
+        
+        # keywords parse
+        with open(self.filepath, "w") as f:
+            json.dump(self.test_data, f)
+
+        with open(self.filepath, "r") as f:
+            loaded_data = cjson.load("dummy", fp=f)
+        
+        self.assertEqual(loaded_data, self.test_data)
+
+    def test_json_dump(self):
+        with open(self.filepath, "w") as f:
+            cjson.dump(self.test_data, f)
+
+        with open(self.filepath, "r") as f:
+            # file_contents = f.read()
+            loaded_data = json.loads(f)
+        # loaded_data = json.load(file_contents)
+
+        self.assertEqual(loaded_data, self.test_data)
+
+        # keywords parse
+        with open(self.filepath, "w") as f:
+            cjson.dump("dummy", fp=f, obj=self.test_data)
+
+        with open(self.filepath, "r") as f:
+            # file_contents = f.read()
+            loaded_data = json.loads(f)
+        # loaded_data = json.load(file_contents)
+
+        self.assertEqual(loaded_data, self.test_data)
+
+if __name__ == "__main__":
+    sys.path.append(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    )
+    unittest.main()
