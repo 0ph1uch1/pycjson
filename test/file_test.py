b691afa3a4d1d59e239ff69be080ac7cce3ef283
helix_xx <630824252@qq.com>
2024-04-23 03:32:25 +0800

    #4 use python tmp file and bench data

--- test/file_test.py
+++ test/file_test.py
@@ -4,19 +4,20 @@ import json
 import unittest
 
 class TestJsonFileIO(unittest.TestCase):
+    def _check_obj_same(self, a, b):
+        from test_utils import check_obj_same
+        return check_obj_same(self, a, b)
+    
     def setUp(self):           
-        self.test_data = {
-            "name": "XXY",
-            "age": 99,
-            "city": "GZ"
-        }
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        json_path = os.path.join(current_dir, '..','bench',"tmp_file.json")
-        self.filepath = json_path
+        from test_utils import get_benchfiles_fullpath
+        self.file_path = get_benchfiles_fullpath()
+        self.datas = []
+        for file in get_benchfiles_fullpath():
+            with open(file, "r", encoding='utf-8') as f:
+                self.datas.append(json.load(f))
 
     def tearDown(self):
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
+        pass
 
     def test_json_load(self):
         import cjson
@@ -32,41 +33,48 @@ class TestJsonFileIO(unittest.TestCase):
                 return A(obj["a"])
             return obj
         
-        with open(self.filepath, "w") as f:
-            json.dump(self.test_data, f)
+        for file in self.file_path:
+            # keywords parse
+            with open(file, "r+") as f:
+                cjson_data = cjson.load(object_hook=d4, fp=f)
+                f.seek(0)
+                json_data = json.load(f)
+            self._check_obj_same(cjson_data, json_data)
 
-        with open(self.filepath, "r") as f:
-            loaded_data = cjson.load(f)
+            with open(file, "r+") as f:
+                cjson_data = cjson.load(f)
+                f.seek(0)
+                json_data = json.load(f)
+            self._check_obj_same(cjson_data, json_data)
         
-        self.assertEqual(loaded_data, self.test_data)
-        
-        # keywords parse
-        with open(self.filepath, "w") as f:
-            json.dump(self.test_data, f)
-
-        with open(self.filepath, "r") as f:
-            loaded_data = cjson.load(object_hook=d4, fp=f)
-        
-        self.assertEqual(loaded_data, self.test_data)
 
     def test_json_dump(self):
         import cjson
-        with open(self.filepath, "w") as f:
-            cjson.dump(self.test_data, f)
+        import tempfile
+        for data in self.datas:
+            with tempfile.NamedTemporaryFile(mode="w+") as f:
+                cjson.dump(data, f)
+                f.seek(0)
+                cjson_data = json.load(f)
+            with tempfile.NamedTemporaryFile(mode="w+") as f:
+                json.dump(data, f)
+                f.seek(0)
+                json_data = json.load(f)
+            self._check_obj_same(cjson_data, json_data)
 
-        with open(self.filepath, "r") as f:
-            loaded_data = json.load(f)
-
-        self.assertEqual(loaded_data, self.test_data)
 
         # keywords parse
-        with open(self.filepath, "w") as f:
-            cjson.dump(skipkeys=False, fp=f, obj=self.test_data)
+        for data in self.datas:
+            with tempfile.NamedTemporaryFile(mode="w+", encoding='utf-8') as f:
+                cjson.dump(skipkeys=False, fp=f, obj=data)
+                f.seek(0)
+                cjson_data = json.load(f)
+            with tempfile.NamedTemporaryFile(mode="w+", encoding='utf-8') as f:
+                json.dump(data, f)
+                f.seek(0)
+                json_data = json.load(f)
+            self._check_obj_same(cjson_data, json_data)
 
-        with open(self.filepath, "r") as f:
-            loaded_data = json.load(f)
-
-        self.assertEqual(loaded_data, self.test_data)
 
 if __name__ == "__main__":
     sys.path.append(
