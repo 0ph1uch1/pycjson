e8aae2af79bff82a401f51348f32a9b9205d4a33
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-04-24 15:11:47 -0400

    fix: follow python utf8 setting
    [skip ci]

--- src/str.h
+++ src/str.h
@@ -3,9 +3,10 @@
 #include <Python.h>
 #include <stdbool.h>
 
+int get_utf8_type(uint32_t unciode_value);
+int get_unicode_value(const char *str, Py_UCS4 *re);
 bool str2unicode_1byte(PyObject **re, const char *str, long alloc, long num);
 bool str2unicode_2byte(PyObject **re, const char *str, long alloc, long num);
-bool str2unicode_3byte(PyObject **re, const char *str, long alloc, long num);
 bool str2unicode_4byte(PyObject **re, const char *str, long alloc, long num);
 
 #endif //STR_H
