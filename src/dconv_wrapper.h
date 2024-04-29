5d1685de0a7b42053075d3321d3d96eb8038f056
antares0982@gmail.com <antares0982@gmail.com>
2024-04-30 02:12:36 +0800

    refactor: move cpp files to deps

--- src/dconv_wrapper.h
+++ src/dconv_wrapper.h
@@ -1,8 +1,12 @@
 #ifndef DCONV_WRAPPER_H
 #define DCONV_WRAPPER_H
 #include <stdbool.h>
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 int dconv_d2s(double value, char *buf, int buflen, int *strlength, bool allow_nan);
-double dconv_s2d(const char *buffer, int length, int *processed_characters_count) ;
-
+double dconv_s2d(const char *buffer, int length, int *processed_characters_count);
+#ifdef __cplusplus
+}
+#endif
 #endif //DCONV_WRAPPER_H
--- src/simdutf_wrapper.h
+++ src/simdutf_wrapper.h
@@ -1,7 +1,12 @@
 #ifndef SIMDUTF_WRAPPER_H
 #define SIMDUTF_WRAPPER_H
 #include <stddef.h>
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 size_t count_utf8(const char *buf, size_t len);
 size_t utf8_length_from_latin1(const char *buf, size_t len);
+#ifdef __cplusplus
+}
+#endif
 #endif //SIMDUTF_WRAPPER_H
