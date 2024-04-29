d6f25f6d94c41aab5316a37ce2ceef729eeca09d
antares0982@gmail.com <antares0982@gmail.com>
2024-04-29 22:45:07 +0800

    build: refactor to use cmake

--- setup.py
+++ setup.py
@@ -1,48 +1,92 @@
 import os
 import platform
+import subprocess
+import sys
 
 from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 
-if platform.system() == "Linux":
-    strip_flags = ["-Wl,--strip-all"]
-else:
-    strip_flags = []
-
-
-def find_src():
-    srcs = []
-    headers = []
-    for root, _, files in os.walk("src"):
-        for file in files:
-            if file.endswith(".c") or file.endswith(".cpp"):
-                srcs.append(os.path.join(root, file))
-            elif file.endswith(".h"):
-                if file != "version_template.h":
-                    headers.append(os.path.join(root, file))
-    srcs.append(os.path.abspath("./deps/double-conversion.cpp"))
-    headers.append(os.path.abspath("./deps/double-conversion.h"))
-    return srcs, headers
-
-
-srcs, headers = find_src()
-
-module1 = Extension(
-    "cjson",
-    sources=srcs,
-    include_dirs=["./src", "./deps"],
-    extra_compile_args=["-D_GNU_SOURCE"],
-    extra_link_args=["-lstdc++", "-lm"] + strip_flags,
-)
 
 with open("src/version_template.h", encoding='utf-8') as f:
     version_template = f.read()
 
 
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', **kwargs):
+        Extension.__init__(self, name, sources=[], **kwargs)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
+
+
+class cmake_build_ext(build_ext):
+    def build_extensions(self):
+        # Ensure that CMake is present and working
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('Cannot find CMake executable')
+
+        for ext in self.extensions:
+
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+            # cfg = 'Debug' if options['--debug'] == 'ON' else 'Release'
+            cfg = 'Release'
+
+            cmake_args = [
+                f'-DCMAKE_BUILD_TYPE={cfg}',
+                # Ask CMake to place the resulting library in the directory
+                # containing the extension
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                # Other intermediate static libraries are placed in a
+                # temporary build directory instead
+                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), self.build_temp),
+                # Hint CMake to use the same Python executable that
+                # is launching the build, prevents possible mismatching if
+                # multiple versions of Python are installed
+                '-DPYTHON_EXECUTABLE={}'.format(sys.executable),
+                # Add other project-specific CMake arguments if needed
+                # ...
+            ]
+
+            # We can handle some platform-specific settings at our discretion
+            if platform.system() == 'Windows':
+                plat = ('x64' if platform.architecture()[0] == '64bit' else 'Win32')
+                cmake_args += [
+                    # These options are likely to be needed under Windows
+                    '-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE',
+                    '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                ]
+                # Assuming that Visual Studio and MinGW are supported compilers
+                if self.compiler.compiler_type == 'msvc':
+                    cmake_args += [
+                        '-DCMAKE_GENERATOR_PLATFORM=%s' % plat,
+                    ]
+                else:
+                    cmake_args += [
+                        '-G', 'MinGW Makefiles',
+                    ]
+            # cmake_args += cmake_cmd_args
+
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+
+            # Config
+            subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args,
+                                  cwd=self.build_temp)
+
+            # Build
+            subprocess.check_call(['cmake', '--build', '.', '--config', cfg],
+                                  cwd=self.build_temp)
+
+
 setup(
-    ext_modules=[module1],
+    name='cjson',
     use_scm_version={
         "local_scheme": lambda x: "",
         "write_to": "src/version.h",
         "write_to_template": version_template,
     },
+    ext_modules=[
+        CMakeExtension(name='cjson')
+    ],
+    cmdclass=dict(build_ext=cmake_build_ext)
 )
